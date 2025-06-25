85befee5cd62b8e0d5a4d1b6b9b071f0e43e9177
lcmongwe <lcmongwe@gmail.com>
2025-06-25 10:56:18 +0300

    Navbar commponet added links

---
+++ app/NavBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+
+const NavBar = () => {
+  return (
+    <nav>
+          <Link href='/'>Logo</Link>
+          <ul>
+              <li><Link href='/'>Dashboard</Link></li>
+               <li><Link href='/issues'>Issues</Link></li>
+          </ul>
+          
+    </nav>
+  )
+}
+
+export default NavBar
